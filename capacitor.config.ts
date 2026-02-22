e865d0d3de411ef7d6c92bd353278c9a1e4d3974
Kajol  Wanode <kajolwanode@gmail.com>
2026-02-22 08:45:34 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saniya.repo',
+  appName: 'Github-Analyzer',
+  webDir: 'dist'
+};
+
+export default config;
